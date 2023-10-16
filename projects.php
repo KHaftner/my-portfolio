1637a0a1aa771b44bba8af5de561e798875778f4
KHaftner <126606040+KHaftner@users.noreply.github.com>
2023-10-16 01:05:39 -0700

    Fiddled around with the polaroid hover.

--- projects.php
+++ projects.php
@@ -27,19 +27,34 @@ error_reporting(E_ALL);
                 </header>
                 <section class="showcased-projects">
                     <article>
-                        <a href="project-movies.html"><img src="images/movie-love-polaroid2.jpg"
+                        <a href="project-movies.html">
+                            <img src="images/movie-polaroid.jpg"
                                 alt="A polaroid image with a preview of the Movie Love website"
-                                class="polaroid-preview movies"></a>
+                                class="polaroid-preview movies bottom">
+                                <img src="images/movie-polaroid2.jpg"
+                                alt="A polaroid image with a preview of the Movie Love website"
+                                class="polaroid-preview movies top-img">
+                        </a>
                     </article>
                     <article>
-                        <a href="project-abby.html"><img src="images/abby-wants-to-play-polaroid2.jpg"
+                        <a href="project-abby.html">
+                            <img src="images/abby-polaroid.jpg"
                                 alt="A polaroid image with a preview of the Abby Wants to Play game"
-                                class="polaroid-preview abby"></a>
+                                class="polaroid-preview abby bottom">
+                                <img src="images/abby-polaroid2.jpg"
+                                alt="A polaroid image with a preview of the Abby Wants to Play game"
+                                class="polaroid-preview abby top-img">
+                        </a>
                     </article>
                     <article>
-                        <a href="project-amazilia.html"><img src="images/amazilia-poraloid2.jpg"
-                                alt="A polaroid image for the Amazilia Acrescapers project that says 'Project Ongoing'"
-                                class="polaroid-preview amazilia"></a>
+                        <a href="project-amazilia.html">
+                            <img src="images/amazilia-polaroid.jpg"
+                                alt="A polaroid image for the Amazilia Acrescapers project"
+                                class="polaroid-preview amazilia bottom">
+                                <img src="images/amazilia-polaroid2.jpg"
+                                alt="A polaroid image for the Amazilia Acrescapers project"
+                                class="polaroid-preview amazilia top-img">
+                        </a>
                     </article>
                 </section>
 
