4abb9e49563a713107042c0fec9e27a2aa65ac2d
KHaftner <126606040+KHaftner@users.noreply.github.com>
2023-10-22 02:28:59 -0700

    Fixed sass compile issues (thanks to chatGPT lol).

--- head.php
+++ head.php
@@ -26,7 +26,7 @@
         href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Shadows+Into+Light&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="styles/output/styles.css" type="text/css" />
+    <link rel="stylesheet" href="styles/styles.css" type="text/css" />
     <link rel="stylesheet" href="node_modules/aos/dist/aos.css" />
 
 </head>
