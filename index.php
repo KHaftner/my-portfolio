b097e072e9a3fc34d83c44d46abaf2d3d760b045
Kassandra Haftner <Kassandra.Haftner@outlook.com>
2024-10-18 05:33:04 -0700

    Below fold style changes.

--- index.php
+++ index.php
@@ -46,25 +46,9 @@ error_reporting(E_ALL);
                 <h2 data-aos="zoom-in-right" data-aos-delay="200">Welcome.</h2>
                 <div class="p-container">
                     <p class="intro-p">
-                        I'm a freelance web developer based in Metro Vancouver, British Columbia.
-                    </p>
-                    <p>I appreciate you taking the time to check out my work! Currently, I'm a student at Thompson Rivers University, working towards a Bachelor of Computing Science.</p>
-                    <p>In addition to my current studies, I have also completed the Front-End Web Developer program at BCIT.</p>
-                    <p>If you're looking for some help or collaboration on a web development project, I'd be happy to help you out.</p>
-
-                    <a href="#skills-list"><svg class="the-down-button" clip-rule="evenodd" fill-rule="evenodd"
-                            stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <title>Down Button</title>
-                            <path
-                                d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z" />
-                        </svg></a>
+                    I appreciate you taking the time to check out my work!<br> I'm a freelance web developer looking to take on some extra projects while I work towards my degree in Computing Science at TRU. In addition to my current studies, I've also completed the Front-End Web Development program at BCIT.</p>
                 </div>
 
-            </div>
-
-            <div class="bottom-img-wrapper">
-                <img src="images/abby-computer.png" alt="A dog sitting at a computer with a cup of coffee" loading="lazy" data-aos="zoom-in-left" data-aos-delay="200" />
             </div>
 
         </section>
@@ -74,57 +58,6 @@ error_reporting(E_ALL);
         <section class="skills-list" id="skills-list">
             <div class="skills-top-wrapper">
                 <h2 data-aos="fade-right" data-aos-delay="200">Toolkit.</h2>
-                <div class="skills-top-svg">
-                    <svg width="900" height="600" viewBox="0 0 900 600" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <title>Code Symbol Drawing</title>
-                        <path fill="transparent" d="M0 0h900v600H0z" />
-                        <path clip-rule="evenodd"
-                            d="M588.179 387.428h42.605c11.759 0 21.303 9.543 21.303 21.302v42.605c0 11.759-9.544 21.302-21.303 21.302h-42.605c-11.759 0-21.302-9.543-21.302-21.302V408.73c0-11.759 9.543-21.302 21.302-21.302zM289.945 89.194h42.605c11.759 0 21.302 9.543 21.302 21.302v42.605c0 11.759-9.543 21.302-21.302 21.302h-42.605c-11.759 0-21.302-9.543-21.302-21.302v-42.605c0-11.759 9.543-21.302 21.302-21.302z"
-                            stroke="#d795ad" stroke-width="8" stroke-linecap="round" stroke-linejoin="round" />
-                        <path clip-rule="evenodd"
-                            d="M439.062 238.311h42.605c11.759 0 21.303 9.543 21.303 21.302v42.605c0 11.759-9.544 21.303-21.303 21.303h-42.605c-11.759 0-21.302-9.544-21.302-21.303v-42.605c0-11.759 9.543-21.302 21.302-21.302z"
-                            stroke="#d795ad" stroke-width="12" stroke-linecap="round" stroke-linejoin="round" />
-                        <path
-                            d="M439.062 131.798h170.42c23.539 0 42.605 19.066 42.605 42.605v63.908c0 23.539-19.066 42.604-42.605 42.604h-42.605m-85.21 149.118H311.248c-23.54 0-42.605-19.066-42.605-42.605V323.52c0-23.539 19.065-42.604 42.605-42.604h42.604"
-                            stroke="#d795ad" stroke-width="8" stroke-linecap="round" stroke-linejoin="round" />
-                        <circle cx="467.383" cy="280.993" r="78.733" fill="#987eb2" stroke="#987eb2"
-                            stroke-width="11.812" stroke-linecap="round" stroke-linejoin="round" />
-                        <path
-                            d="m475.715 251.834-16.663 58.321m-18.756-20.832-16.656-16.656 16.656-16.666m54.159 49.987 16.665-16.665-16.665-16.656"
-                            stroke="#fff" stroke-width="9.45" stroke-linecap="round" stroke-linejoin="round" />
-                        <rect x="692.86" y="131.702" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="669.726" y="299.423" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="97.163" y="206.887" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="89.066" y="445.166" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="692.86" y="169.873" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="669.726" y="337.593" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="97.163" y="245.058" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <rect x="89.066" y="483.337" width="158.467" height="7.35" rx="3.675" fill="#d795ad" />
-                        <path
-                            d="M764.964 231.931c15.739-3.729 32.386 9.619 32.386 9.619s-8.884 19.386-24.628 23.103c-15.739 3.729-32.381-9.607-32.381-9.607s8.884-19.386 24.623-23.115z"
-                            fill="url(#a)" />
-                        <circle cx="207.865" cy="321.348" r="15.854" fill="#987eb2" />
-                        <circle r="11.903" transform="matrix(1 0 0 -1 840.096 236.43)" fill="#987eb2" />
-                        <circle r="11.903" transform="matrix(1 0 0 -1 584.467 186.693)" fill="#987eb2" />
-                        <circle r="22.725" transform="matrix(1 0 0 -1 326.936 505.194)" fill="#987eb2" />
-                        <circle r="12.683" transform="matrix(1 0 0 -1 609.537 430.377)" fill="#d795ad" />
-                        <circle r="10.821" transform="matrix(1 0 0 -1 439.955 494.447)" fill="#d795ad" />
-                        <circle r="10.575" transform="scale(-1 1) rotate(-75 -106.451 688.544)" fill="#d795ad" />
-                        <circle r="14.068" transform="matrix(1 0 0 -1 729.916 108.021)" fill="#d795ad" />
-                        <ellipse rx="10.821" ry="8.657" transform="matrix(1 0 0 -1 59.403 293.31)" fill="#d795ad" />
-                        <circle r="15.545" transform="scale(-1 1) rotate(-75 -45.443 183.402)" fill="#d795ad" />
-                        <path
-                            d="M129.51 372.474h-.283c-1.677-23.752-19.341-24.117-19.341-24.117s19.479-.38 19.479-27.825c0 27.445 19.478 27.825 19.478 27.825s-17.657.365-19.333 24.117zm14.285-231.006h-.205c-1.217-17.696-14.04-17.968-14.04-17.968s14.14-.284 14.14-20.73c0 20.446 14.139 20.73 14.139 20.73s-12.817.272-14.034 17.968z"
-                            fill="#d795ad" />
-                        <defs>
-                            <linearGradient id="a" x1="824.233" y1="222.66" x2="686.453" y2="287.752"
-                                gradientUnits="userSpaceOnUse">
-                                <stop stop-color="#fff" />
-                                <stop offset="1" stop-color="#EEE" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                </div>
             </div>
 
             <div class="filter-buttons">
